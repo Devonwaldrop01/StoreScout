5773af3e5a0b3d1c46a37828708df18331a3f7cb
devonwaldrop01 <devonwaldrop0131@gmail>
2026-05-11 19:35:58 -0400

    landing page added

--- app/main.py
+++ app/main.py
@@ -220,7 +220,7 @@ def fenty_pricing_strategy():
     article_path = TEMPLATES_DIR / "how-to-analyze-shopify-competitors.html"
     return HTMLResponse(article_path.read_text(encoding="utf-8"))
 
-@app.get("/landing-page", response_class=HTMLResponse)
+@app.get("/landing", response_class=HTMLResponse)
 def fenty_pricing_strategy():
     article_path = TEMPLATES_DIR / "landing-page.html"
     return HTMLResponse(article_path.read_text(encoding="utf-8"))
